7643a3a8643ca2b5cb7aef5854a7bc6fa7f6ffce
Hiago Chagas <hiago.chagas@premiersoft.net>
2023-04-18 09:11:50 -0300

    Initial Commit

---
+++ heartMonitoriOS/heartMonitoriOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  heartMonitoriOSApp.swift
+//  heartMonitoriOS
+//
+//  Created by PremierSoft on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct heartMonitoriOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
