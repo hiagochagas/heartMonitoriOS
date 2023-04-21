0cab96488db21e9a3fe4906a6d240de24323540d
Hiago Chagas <hiago.chagas@premiersoft.net>
2023-04-21 17:47:16 -0300

    moved views to computed properties in HomeView

--- heartMonitoriOS/HomeView.swift
+++ heartMonitoriOS/HomeView.swift
@@ -9,11 +9,24 @@ import SwiftUI
 
 struct HomeView: View {
     @StateObject private var viewModel = HomeViewModel()
+    @State private var animationAmount: CGFloat = 1
+    
+    private var heartRateText: Text {
+        let heartRate = viewModel.monitoring?.heartRate ?? 0
+        let text = String(format: "Frequência cardíaca: %.1f bpm", heartRate)
+        return Text(text)
+    }
+    
+    private var sp02Text: Text {
+        let sp02 = viewModel.monitoring?.sp02 ?? 0
+        return Text("Oxigenação sanguínea: \(sp02) %")
+    }
+    
     
     var body: some View {
         VStack(spacing: 20) {
-            Text("Heart rate: \(viewModel.monitoring?.heartRate ?? 0)" )
-            Text("SP02: \(viewModel.monitoring?.sp02 ?? 0)")
+            heartRateText
+            sp02Text
         }
         .padding()
     }
